e787ce49a847f22733422a37d6cccbc832adf5c5
Saulius Skeirys <saulius.skeirys@goodeggs.com>
2021-10-19 09:22:44 -0500

    chore: added @goodeggs/tsconfig, fixed spacing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-export const EMAIL_REGEX: RegExp = /^[^@\s]+@[^@\s]+\.[^@^.\s]+$/;
+export const EMAIL_REGEX = /^[^@\s]+@[^@\s]+\.[^@^.\s]+$/;
+export const OBJECTID_REGEX = /^[0-9a-f]{24}$/;
 
 export function email (str: string): boolean {
   return EMAIL_REGEX.test(str);
 }
-
-export const OBJECTID_REGEX: RegExp = /^[0-9a-f]{24}$/;
 
 export function objectid (str: string): boolean {
   return OBJECTID_REGEX.test(str);
