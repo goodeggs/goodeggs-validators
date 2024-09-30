d555e70600ea41e53b4b8e26d8e1558aca491d71
Guilherme Vieira <guivpw68@gmail.com>
2024-09-30 11:55:49 -0300

    feat: add new email regex

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export const EMAIL_REGEX = /^[^@\s]+@[^@\s]+\.[^@^.\s]+$/;
+export const EMAIL_REGEX = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 export const OBJECTID_REGEX = /^[0-9a-f]{24}$/;
 
 export function email(str: string): boolean {
